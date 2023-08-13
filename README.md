364449877cf706ecd897e67f2e52fe66faa44270
Ajitesh Bhan <ajiteshbhan@gmail.com>
2023-08-13 19:45:17 +0200

    'Model_prepearation'

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
-# Chicken-Disease-Classification-ProjectUpdate config.yaml
+# Chicken-Disease-Classification-Project
+
+
+Project workflow
+
+template.py - Directories and files created for this project
+params.py - Parameters needed for the model
+setup.py - setuptools version and module creation
+research - trials and experiments
+logs -     logs for this project
+src/CNNClassifier- al the source code for the project 
+
 Update secrets.yaml [Optional]
 Update params.yaml
 Update the entity
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from CNNClassifier.pipeline.stage_01_data_ingestion import  DataIngestionStage
 from  CNNClassifier import logger
-
+from CNNClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelStage
 
 stage_name ='Data Ingestion'
 
@@ -11,3 +11,20 @@ try:
     logger.info(f'>>>>>>>> stage {stage_name} completed! <<<<<<<<< ')
 except Exception as e:
     logger.error(f'Error in stage {stage_name}: {e}')
+
+
+
+
+Stage_Name = 'Prepare Base Model'
+
+try:
+    logger.info(f'>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
+    logger.info(f'><<<<<Prepare base model stage {Stage_Name} satrted')    
+    obj= PrepareBaseModelStage()    
+    obj.main()
+    logger.info(f'<<<stage {Stage_Name} completed')
+
+    logger.info(f'<<<<<<<<<<<<<<<<<<<<<<<<<<<<')
+
+except Exception as e:
+    logger.exception(e) 
